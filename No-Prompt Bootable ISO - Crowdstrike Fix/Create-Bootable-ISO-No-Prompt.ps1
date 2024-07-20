63cd7a0ae948a0263f6d9f9f2c428c98a5bdcc52
Brooks Peppin <33036379+brookspeppin@users.noreply.github.com>
2024-07-20 14:32:48 -0600

    Create Create-Bootable-ISO-No-Prompt.ps1

---
+++ No-Prompt Bootable ISO - Crowdstrike Fix/Create-Bootable-ISO-No-Prompt.ps1
@@ -0,0 +1,96 @@
+#Initializing Variables
+
+$WinPE_amd64 = "C:\WinPE_amd64"
+
+$source = "C:\WinPE_amd64\boot.wim"
+
+$mountpath = "$WinPE_amd64\Mount"
+
+$downloads = "$home\Downloads"
+
+ 
+
+#Download and Installing ADK
+
+Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=2196127 -OutFile "$downloads\adksetup.exe"
+
+start-process -FilePath "$downloads\adksetup.exe" -ArgumentList " /features OptionId.DeploymentTools" -Wait
+
+
+#Download and Install Win11 22H2 WinPE ADK (Backwards compatible with win10)- https://go.microsoft.com/fwlink/?linkid=2196224
+
+Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=2196224 -OutFile "$downloads\adkwinpesetup.exe"
+
+start-process -FilePath "$downloads\adkwinpesetup.exe" -ArgumentList "/quiet /features OptionId.WindowsPreinstallationEnvironment" -Wait
+
+ 
+
+#Setup WinPE folders
+
+$env = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
+
+cmd.exe /c """$env"" && copype amd64 $WinPE_amd64"
+
+ 
+
+#Mount Dism and Create Startnet.cmd
+
+Dism /Mount-Image /ImageFile:$source /index:1  /MountDir:$mountpath
+
+'wpeinit' | out-file "$mountpath\windows\system32\startnet.cmd" -Force -Encoding ASCII
+
+$(@('@for %%a in (C D E F G H) do @if exist %%a:\Windows\System32\drivers\CrowdStrike\ set WINDIR=%%a')) | out-file "$mountpath\windows\system32\startnet.cmd" -Force -Encoding ASCII -Append
+
+'del %WINDIR%:\Windows\System32\drivers\CrowdStrike\C-00000291*.sys' | out-file "$mountpath\windows\system32\startnet.cmd" -Append -Encoding ASCII
+
+ 
+
+#Add key components
+
+$packagepath = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\WinPE-WMI.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\en-us\WinPE-WMI_en-us.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\WinPE-NetFX.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\en-us\WinPE-NetFX_en-us.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\WinPE-Scripting.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\en-us\WinPE-Scripting_en-us.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\WinPE-PowerShell.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\en-us\WinPE-PowerShell_en-us.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\WinPE-StorageWMI.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\en-us\WinPE-StorageWMI_en-us.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\WinPE-DismCmdlets.cab"
+
+Dism /Add-Package /Image:"$mountpath" /PackagePath:"$packagepath\en-us\WinPE-DismCmdlets_en-us.cab"
+
+ 
+
+#Save and Unmount Image
+
+Dism /Unmount-Image /MountDir:$mountpath /commit
+
+ 
+
+#make it no prompt
+
+$ADKPath = (Get-ItemProperty -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows Kits\Installed Roots").KitsRoot10 + "Assessment and Deployment Kit\Deployment Tools"
+
+$oscdimg = $ADKPath + "\amd64\Oscdimg\oscdimg.exe"
+
+$etfsboot = $ADKPath + "\amd64\Oscdimg\etfsboot.com"
+
+$efisys_noprompt = $ADKPath + "\amd64\Oscdimg\efisys_noprompt.bin"
+
+$parameters = "-bootdata:2#p0,e,b""$etfsboot""#pEF,e,b""$efisys_noprompt"" -u1 -udfver102 ""$WinPE_amd64\media"" ""$WinPE_amd64\crowdstrike_noprompt.iso"""
+
+$ProcessingResult = Start-Process -FilePath $oscdimg -ArgumentList $parameters -Wait -NoNewWindow -PassThru
